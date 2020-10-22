042260dbadc459967e2740b204530a036b1f404f
zhangliying <zhangliying@wangdian.cn>
2020-10-22 18:07:31 +0800

    九九乘法表使用制表符

--- code/day01/homework/main.go
+++ code/day01/homework/main.go
@@ -75,7 +75,7 @@ func main() {
 	// 4. 久久乘法表
 	for i := 1; i < 10; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Printf("%d*%d=%d ", j, i, j*i)
+			fmt.Printf("%d*%d=%d\t", j, i, j*i)
 		}
 		fmt.Println()
 	}
